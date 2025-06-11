56df1265f1e5ab00ec9b8b67d28fab5e7d4537a6
honopio <honorine.piolat@gmail.com>
2025-06-11 13:42:02 +0200

    fix: pass mongodb URI to connectToDatabase

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import { connectToDatabase, getDb } from './db/database';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const mongodb_uri = process.env.MONGODB_URI || "";
 
 app.get('/api/polls', async (req, res) => {
   const db = getDb();
@@ -11,7 +12,7 @@ app.get('/api/polls', async (req, res) => {
   res.json(polls).status(200);
 });
 
-connectToDatabase().then(() => {
+connectToDatabase(mongodb_uri).then(() => {
   app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
   });

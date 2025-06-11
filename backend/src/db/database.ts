d2e5283dd3f8ddd3f5f06c317f019c3f0bc0b21e
honopio <honorine.piolat@gmail.com>
2025-06-11 13:35:51 +0200

    feat: add schema validation for polls collection in MongoDB

--- backend/src/db/database.ts
+++ backend/src/db/database.ts
@@ -15,3 +15,41 @@ export function getDb(): mongodb.Db {
   if (!db) throw new Error('Database not connected!');
   return db;
 }
+
+async function applySchemaValidation(db: mongodb.Db) {
+  const jsonSchema = {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["title", "options"],
+      properties: {
+        _id: {},
+        title: {
+          bsonType: "string",
+          description: "'title' is required and is a string",
+        },
+        options: {
+          bsonType: "array",
+          items: {
+            bsonType: "object",
+            required: ["text", "votes"],
+            properties: {
+              text: { bsonType: "string" },
+              votes: { bsonType: "int" }
+            }
+          }
+        }
+      }
+    }
+  };
+
+  //try applying the modification to the collection, and creates it if it doesn't exist
+  await db.command({
+    collMod: "polls",
+    validator: jsonSchema
+  })
+  .catch(async (error: mongodb.MongoServerError) => {
+    if (error.codeName === 'NamespaceNotFound') {
+      await db.createCollection("polls", {validator: jsonSchema });
+    }
+  })
+}

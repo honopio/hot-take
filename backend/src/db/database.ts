9c09f93415cef4eafdcedf74301c223c9317ff64
honopio <honorine.piolat@gmail.com>
2025-06-11 13:31:59 +0200

    refactor: update import statements for mongodb types

--- backend/src/db/database.ts
+++ backend/src/db/database.ts
@@ -1,9 +1,9 @@
-import { MongoClient, Db } from 'mongodb';
+import * as mongodb from 'mongodb';
 
 const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/polls'
-const client = new MongoClient(uri);
+const client = new mongodb.MongoClient(uri);
 
-let db: Db;
+let db: mongodb.Db;
 
 export async function connectToDatabase() {
   await client.connect();
@@ -11,7 +11,7 @@ export async function connectToDatabase() {
   console.log('Connected to MongoDB');
 }
 
-export function getDb(): Db {
+export function getDb(): mongodb.Db {
   if (!db) throw new Error('Database not connected!');
   return db;
 }

2b7498977ccb620cd13333709365e86b4fc3c2ae
honopio <honorine.piolat@gmail.com>
2025-06-12 16:54:36 +0200

    feat: add MatIcon import to home component

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrls: ['./home.scss'],
 })
-export class Home {
-
-}
+export class Home {}

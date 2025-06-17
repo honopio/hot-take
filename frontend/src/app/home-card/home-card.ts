ef5edf5ab253f447a1f82eed1198a601d9b2c469
honopio <honorine.piolat@gmail.com>
2025-06-17 13:12:50 +0200

    Merge branch 'fix/home-page'

---
+++ frontend/src/app/home-card/home-card.ts
@@ -0,0 +1,24 @@
+import { Component, input } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-home-card',
+  imports: [MatIcon],
+  template: `<div class="rounded-xl p-8 border border-text h-full">
+    <div
+      class="w-16 h-16 bg-{{
+        color()
+      }} rounded-full border border-black flex items-center justify-center mb-6 mx-auto"
+    >
+      <mat-icon fontIcon="{{ icon() }}" style="color: black"></mat-icon>
+    </div>
+    <h3 class="text-center">{{ title() }}</h3>
+    <p class="text-secondary-text text-center">{{ description() }}</p>
+  </div>`,
+})
+export class HomeCard {
+  icon = input('bar_chart');
+  title = input('Polls');
+  description = input('Create and manage polls easily.');
+  color = input('primary');
+}
--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { HomeCard } from '../home-card/home-card';
 
 @Component({
   selector: 'app-home',
-  imports: [MatIcon, RouterLink],
+  imports: [MatIcon, RouterLink, HomeCard],
   templateUrl: './home.html',
   styleUrls: ['./home.scss'],
 })
-export class Home {}
+export class Home {
+  private router = inject(Router);
+
+  joinPoll(pollInput: string) {
+    let pollId = pollInput.trim();
+    const match = pollId.match(/\/poll\/([a-zA-Z0-9]+)/);
+    if (match) {
+      pollId = match[1]; // only extract the id part (between () in the regex)
+    }
+    this.router.navigate(['/poll', pollId]);
+  }
+}
